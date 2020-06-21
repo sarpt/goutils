eed106facfbb7ad2320cc2c0e85e14c088c392d1
sarpt <mike.m.info@gmail.com>
2020-06-21 15:18:41 +0200

    Changed names to not stutter

--- pkg/listflag/listflag.go
+++ pkg/listflag/listflag.go
@@ -9,28 +9,28 @@ const (
 	stringSeparator = ", "
 )
 
-// ListFlag can be used with std flag package to be used while setting multiple process arguments by repeating the named argument
-type ListFlag struct {
+// StringList can be used with std flag package to be used while setting multiple process arguments by repeating the named argument
+type StringList struct {
 	values        []string
 	allowedValues map[string]bool
 }
 
-// NewListFlag creates a new ListFlag and sets allowed values for a flag
+// NewStringList creates a new ListFlag and sets allowed values for a flag
 // If list of allowed values contains no values then all possible string values are allowed
-func NewListFlag(allowedValues []string) ListFlag {
+func NewStringList(allowedValues []string) StringList {
 	allowed := make(map[string]bool)
 
 	for _, value := range allowedValues {
 		allowed[value] = true
 	}
 
-	return ListFlag{
+	return StringList{
 		allowedValues: allowed,
 	}
 }
 
 // Strings returns string representation of values that were set
-func (f *ListFlag) String() string {
+func (f *StringList) String() string {
 	if f == nil {
 		return ""
 	}
@@ -40,7 +40,7 @@ func (f *ListFlag) String() string {
 
 // Allowed returns whether the value is allowed to be set
 // Always returns true if no allowed values were provided
-func (f ListFlag) Allowed(value string) bool {
+func (f StringList) Allowed(value string) bool {
 	if len(f.allowedValues) == 0 {
 		return true
 	}
@@ -51,18 +51,18 @@ func (f ListFlag) Allowed(value string) bool {
 }
 
 // Values returns set values of the flag
-func (f ListFlag) Values() []string {
+func (f StringList) Values() []string {
 	return f.values
 }
 
 // IsBoolFlag returns false as the list flag is not a bool flag; used by the flag package
-func (f ListFlag) IsBoolFlag() bool {
+func (f StringList) IsBoolFlag() bool {
 	return false
 }
 
 // Set is used during setting of flags when used with flag package
 // When the value is invalid (not allowed), error is being returned
-func (f *ListFlag) Set(value string) error {
+func (f *StringList) Set(value string) error {
 	if !f.Allowed(value) {
 		return fmt.Errorf("value %s is not allowed", value)
 	}
